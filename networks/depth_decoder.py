02d1a6f8bb264971a6a9b19628ea2f548cc3eec2
hanwencheng <605015344@qq.com>
2022-11-14 19:23:12 +0800

    Init commit

--- networks/depth_decoder.py
+++ networks/depth_decoder.py
@@ -70,5 +70,4 @@ class DepthDecoder(nn.Module):
                 self.outputs[("disp", i)] = pred
                 previous_disp = pred
 
-        self.outputs[("disp", 0)] = F.interpolate(self.outputs[("disp", 0)], scale_factor=0.5, mode="bilinear")
         return self.outputs
